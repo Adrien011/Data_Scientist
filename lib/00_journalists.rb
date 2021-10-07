c08829fbaa3dd487c0b55af88d07c7e14f808f17
Adrien <adrien.kired@gmail.com>
2021-10-07 13:40:15 -0700

    Data_Scientist

---
+++ lib/00_journalists.rb
@@ -0,0 +1,140 @@
+
+data = Array.new
+data = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def data_length(data)
+    number=data.count
+    puts number
+    return number
+
+end
+
+def count_string(data)
+    mot=data.min_by(&:length)
+    puts mot
+end
+
+
+def count_five(data)
+    nombre=381
+    total=0
+    i=0
+    while i<nombre
+         number=data[i].size
+           
+        if number == 6
+            total+=1
+
+        end
+        i+=1
+    end
+    puts total
+end
+def maj(data)
+    nombre=381
+    i=0
+    total=0
+    lowerCase=0
+    while i<nombre
+        string=data[i]
+        lettre =string[1].count("A-Z")
+         
+         if lettre==1
+            
+            total+=1
+        end
+        i+=1
+    end
+    puts total
+end
+
+
+def sort(data) 
+     nombre =381
+     dataSort= Array.new
+     i=0
+     while i<nombre
+         string=data[i]
+        dataSort[i] = string
+        i+=1 
+     end
+     dataSort.sort!
+     puts dataSort
+end
+
+def decroissant(data)
+    puts data.sort_by(&:length)
+end
+
+def position(data)
+    nombre=381
+    i=0
+    while i<nombre
+        string =data[i]
+        if string == "@epenser"
+             position =i     
+        end
+        i+=1
+        
+    end
+    puts " voici la position de penser#{position}"
+end
+
+def trier(data)
+    nombre=381
+    i=0
+    j=0
+    count =0
+    tableau =Array.new
+    while j<=data.map(&:length).max 
+        i=0
+        while i<nombre
+            if  data[i].size == j
+                  count +=1
+            end
+            i+=1
+        end
+        puts " voici le nombre handle#{count} pour autant de caractère #{j}"
+        count =0
+        j+=1
+    end
+    
+end
+
+
+def menu (data)
+    chiffre= 0
+    while chiffre != 9
+        puts  "voici la  liste du menu"
+        puts  " tapez  1  pour Combien y a-t-il de handle dans cette array ? "
+        puts  " tapez 2 Quel est le handle le plus court de cette liste ?"
+        puts   "tapez 3 Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère) "
+        puts  "tapez 4 Combien commencent par une majuscule (première lettre juste après le @) ?"
+        puts   " tapez 5 Trie la liste de handle par ordre alphabétique. "
+        puts  "tapez 6 Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)"
+        puts  "tapez 7 Quelle est la position dans l'array de la personne @epenser ? "
+        puts  " tapez 8 Sors-moi une répartition des handle par taille de ces derniers"
+        puts   "tapez 9 pour quittez"
+        chiffre = gets.chomp.to_i
+        if chiffre == 1
+            data_length(data)
+        
+        elsif chiffre ==2
+            count_string(data)
+
+        elsif chiffre ==3
+            count_five(data)
+        elsif chiffre ==4
+            maj(data)
+        elsif chiffre ==5 
+            sort(data)
+        elsif chiffre ==6
+            decroissant(data)
+        elsif chiffre ==7
+            position(data)
+        elsif chiffre ==8
+            trier(data)
+        end
+    end
+end
+menu(data)
